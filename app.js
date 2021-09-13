c3b0ab33e46907797c749ca749404a0d6e2a783e
Thomas Catchlove <thomascatchlove@192-168-1-101.tpgi.com.au>
2021-09-13 19:51:32 +1000

    create ai winner logic

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-console.log("app")
+const aiNumberedButtons = ['.one', '.two', '.three', '.four', '.five', '.six', '.seven', '.eight', '.nine', '.ten', '.eleven', '.twelve']
 
-const aiNumberedButtons = ['.one', '.two', '.three', '.four', '.five', '.six', '.seven', '.eight', '.nine', '.ten', '.eleven', '.twelve', '.thirteen', '.fourteen', '.fifteen', '.sixteen']
+const playerNumberedButtons = ['.thirteen', '.fourteen', '.fifteen', '.sixteen', '.seventeen', '.eighteen', '.nineteen', '.twenty', '.t-one', '.t-two', '.t-three', '.t-four']
 
-const playerNumberedButtons = ['.seventeen', '.eighteen', '.nineteen', '.twenty', '.t-one', '.t-two', '.t-three', '.t-four', '.t-five', '.t-six', '.t-seven', '.t-eight', '.t-nine', '.thirty', '.th-one', '.th-two']
+const aiColours = ['#e6194b', '#000075', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff']
 
-const aiColours = ['#e6194b', '#000075', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3']
+const playerColours = ['#e6194b', '#000075', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff']
 
-const playerColours = ['#e6194b', '#000075', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3']
-
-
+const middlePanel = document.querySelector('.middle-panel')
 
 const targetColour = '#e6194b'
-
-
 
 
 
@@ -61,7 +57,7 @@ const changeColour = e => {
     if (playerNumberedButtons.includes('.' + e.target.className.slice(4))) {
         e.target.style.backgroundColor = playerRandomColour
         newPlayerColours.splice(playerNumberedButtons.indexOf('.' + e.target.className.slice(4)), 1, playerRandomColour)
-        console.log(newPlayerColours)
+        // console.log(newPlayerColours)
     }
 }
 
@@ -75,74 +71,37 @@ for (let i = 0; i < btns.length; i++) {
 }
 
 
-// Used like so
-
-
-// console.log(playerColours);
-
 let displayTargetColour = document.querySelector('.middle-panel').style.backgroundColor = targetColour
 
-// document.querySelector(aiNumberedButtons[0]).style.backgroundColor = 'blue'
-
-
-
-// let testHex = rgb2hex(testColor)
-
-
-// while (newAiColours[0] !== targetColour) {
-
-//     aiRandomColour = aiColours[Math.floor(Math.random() * aiColours.length)]
-
-//     // newAiColours[0].style.backgroundColor = aiRandomColour
-
-//     document.querySelector(aiNumberedButtons[0]).style.backgroundColor = aiRandomColour
-
-// }
-
-// for (let i = 0; i < aiColours.length; i++) {
-//     document.querySelector(aiNumberedButtons[0]).style.backgroundColor = aiColours[i]   
-// }
-
-
-
-
-
-// setInterval(
-// function () {
-//     let colour = newAiColours[0]
-//     document.querySelector(aiNumberedButtons[0]).style.backgroundColor  = colour;
-
-//     if (colour === targetColour) return
-//     newAiColours.shift()
-// },1000);
 
 let idx = 0
 
-setInterval(
-function () {
-    let colour = aiColours[Math.floor(Math.random() * aiColours.length)]
-    document.querySelector(aiNumberedButtons[idx]).style.backgroundColor = colour;
-    
-    if (colour === targetColour) {
-        idx += 1
-    return
-    }
-    newAiColours.shift()
+const startGame = () => {
 
-},500);
+    const aiPlay = setInterval(
+        function () {
+            if (idx < 12) {
+                console.log(idx)
+                let colour = aiColours[Math.floor(Math.random() * aiColours.length)]
+                document.querySelector(aiNumberedButtons[idx]).style.backgroundColor = colour
+     
+                if (colour === targetColour) {
+                    idx += 1
+                }
+                newAiColours.shift()
+            } else {
+                console.log('game over')
+                clearInterval(aiPlay)
+            }
+
+        },
+    50)
+}
 
 
-// for (let i = 0; i < aiNumberedButtons.length; i++) {
-//     setInterval(
-//         function () {
-//             let colour = newAiColours[0]
-//             document.querySelector(aiNumberedButtons[i]).style.backgroundColor = colour;
-        
-//             if (colour === targetColour) return
-//             newAiColours.shift()
-//         },1000);
-// }
 
+
+middlePanel.addEventListener('click', startGame)
 
 
     
