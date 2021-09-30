1020b8f1448225c64d377c7578414919416859e5
Thomas Catchlove <thomascatchlove@192-168-1-101.tpgi.com.au>
2021-09-30 14:45:35 +1000

    finalise MVP

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-const aiNumberedButtons = ['.one', '.two', '.three', '.four', '.five', '.six', '.seven', '.eight', '.nine', '.ten', '.eleven', '.twelve']
+const computerBrickClasses = ['.one', '.two', '.three', '.four', '.five', '.six', '.seven', '.eight', '.nine', '.ten', '.eleven', '.twelve']
 
-const playerNumberedButtons = ['.thirteen', '.fourteen', '.fifteen', '.sixteen', '.seventeen', '.eighteen', '.nineteen', '.twenty', '.t-one', '.t-two', '.t-three', '.t-four']
+const playerBrickClasses = ['.thirteen', '.fourteen', '.fifteen', '.sixteen', '.seventeen', '.eighteen', '.nineteen', '.twenty', '.t-one', '.t-two', '.t-three', '.t-four']
 
 let playerColours = ['#e6194b', '#000075', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff']
-let aiColours = ['#e6194b', '#000075', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff']
+let computerColours = ['#e6194b', '#000075', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff']
 let targetColours = ['#e6194b', '#000075', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff']
 
-let aiPlay 
-
-const middlePanel = document.querySelector('.middle-panel')
-
-
-
-
-let middlePanelClicked = false
-
-let gameOver = false
-
 const shuffle = array => {
+
     let currentIndex = array.length,  randomIndex
     
     while (currentIndex != 0) {
@@ -32,151 +22,160 @@ const shuffle = array => {
     return array
 }
 
+let targetColour
+const middlePanel = document.querySelector('.middle-panel')
+let middlePanelClicked = false
+let currentPlayerColours 
+let computerInPlay
+let gameOver = false
+let playerColourOptions = []
+let randomPlayerColour
+let playerClickCount = 0
+let gameSpeed = 350 
+let totalNumberOfComputerBricks = 12
+let computerBrickIdx = 0
+let limitOfClicksBeforePlayerColourOptionsIsReduced = 20
+let classNameNumberPosition = 6
+let randomLimitOfClicks
 
-    
-    
-let newAiColours = [...aiColours]
+const createPlayerColourOptions = () => {
 
-let newPlayerColours = [...playerColours]
-
-shuffle(targetColours)
-
-let targetColour = targetColours[0]
-
-let displayTargetColour = middlePanel.style.backgroundColor = targetColour 
-
-
-
-
-
-
-// const refreshGame = () => {
-
-//     shuffle(aiColours)
-//     shuffle(playerColours)
-
-//     newPlayerColours = [...playerColours]
-
-//     aiNumberedButtons.forEach((btn, i)=> {
-//         document.querySelector(btn).style.backgroundColor = aiColours[i]
-//     })
-    
-//     playerNumberedButtons.forEach((btn, i)=> {
-//         document.querySelector(btn).style.backgroundColor = playerColours[i]
-//     })
-
-//     let randomColour = shuffle(targetColours)
-//     targetColour = randomColour[0]
-
-//     document.querySelector('.middle-panel').style.backgroundColor = targetColour
-//     gameOver = false
-
-//     startGame()
-    
-// }    
-
-
-const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
-
-let playerRandomColour = playerColours[Math.floor(Math.random() * playerColours.length)] 
-
-const changeColour = e => {
-
-    if (middlePanelClicked === true) {
-        playerRandomColour = playerColours[Math.floor(Math.random() * playerColours.length)] 
-
-        let targetClassName = '.' + e.target.className.slice(4)
-    
-        if (playerNumberedButtons.includes(targetClassName)) {
-            e.target.style.backgroundColor = playerRandomColour
-            newPlayerColours.splice(playerNumberedButtons.indexOf(targetClassName), 1, playerRandomColour)
-
-            console.log(listOfWrongColours())
-    
-            if (listOfWrongColours().length === 0) {
-                clearInterval(aiPlay)
-                gameOver = true
-                middlePanelClicked = false
-                console.log("game over player wins")
-            }
-        }    
-    }    
+    playerColours.forEach(colour => {
+        if (colour !== targetColour) playerColourOptions.push(colour)  
+    })
+    // Add target colour to end of array
+    playerColourOptions.push(targetColour)
 }
 
-const btns = document.querySelectorAll('.btn')
+const displayDefualtPage = () => {
 
+    shuffle(targetColours)
+    targetColour = targetColours[0]
+    middlePanel.style.backgroundColor = targetColour 
+    
+    shuffle(computerColours)
+    computerBrickClasses.forEach((brick, i)=> {
+        document.querySelector(brick).style.backgroundColor = computerColours[i]
+    })
 
-for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", changeColour)
+    shuffle(playerColours)
+    playerBrickClasses.forEach((brick, i)=> {
+        document.querySelector(brick).style.backgroundColor = playerColours[i]
+    })    
+
+    currentPlayerColours = [...playerColours]
+
+    createPlayerColourOptions()
+
+    // console.log(gameSpeed)
 }
 
-
-
-
+displayDefualtPage()
 
 
 const startGame = () => {
 
-    if (gameOver === false && middlePanelClicked === false) {
-        let idx = 0
-        middlePanelClicked = true
-        aiPlay = setInterval(
-            function () {
-                if (idx < 12) {
-                    let colour = aiColours[Math.floor(Math.random() * aiColours.length)]
-                    document.querySelector(aiNumberedButtons[idx]).style.backgroundColor = colour
-         
-                    if (colour === targetColour) {
-                        idx += 1
-                    }
-                    newAiColours.shift()
-                } else {
-                    gameOver = true
-                    middlePanelClicked = false
-                    console.log('game over computer wins')
-                    clearInterval(aiPlay)
+    if (middlePanelClicked === false && gameOver === true) {
+        computerBrickIdx = 0
+        displayDefualtPage()
+        startComputerInPlay()
+    } else if (middlePanelClicked === false) {
+        startComputerInPlay()
+    }
+}
+
+const increaseGameSpeed = () => {
+
+    gameSpeed -= 15
+}
+
+const declareWinner = winner => {
+
+    console.log(`game over ${winner} wins`)
+    middlePanelClicked = false
+    gameOver = true
+    clearInterval(computerInPlay)
+
+    if (winner === 'player') {
+       increaseGameSpeed()
+    } 
+}
+
+const moveToNextBrick = () => {
+
+    computerBrickIdx += 1
+}
+
+const startComputerInPlay = () => {
+
+    middlePanelClicked = true
+    gameOver = false
+
+    computerInPlay = setInterval(
+        function () {
+            if (computerBrickIdx < totalNumberOfComputerBricks) {
+                let randomComputerColour = computerColours[Math.floor(Math.random() * computerColours.length)]
+                document.querySelector(computerBrickClasses[computerBrickIdx]).style.backgroundColor = randomComputerColour
+        
+                if (randomComputerColour === targetColour) {
+                    moveToNextBrick()
                 }
-    
-            },
-        900)
-    } else if (middlePanelClicked === false)(
-        refreshGame()
-    )
+            } else {
+                declareWinner('computer')
+            }
+        },
+    gameSpeed)
+}
+
+const allEqual = arr => arr.every(v => v === arr[0])
+
+const checkIfPlayerWon = () => {
+
+    if (currentPlayerColours[0] === targetColour && allEqual(currentPlayerColours)) {
+        declareWinner('player')
+    }   
+}
 
 
+const reducePlayerColourOptions = () => {
+    if (playerClickCount > randomLimitOfClicks && playerColourOptions[0] != targetColour) {
+        playerColourOptions.shift()
+        playerClickCount = 0
+    }
+}
+
+
+const changePlayerBrickColour = event => {
+
+    randomLimitOfClicks = Math.round(Math.random() * limitOfClicksBeforePlayerColourOptionsIsReduced)
+
+    if (middlePanelClicked === true) {
+
+        let targetClassName = '.' + event.target.className.slice(classNameNumberPosition)
+        playerClickCount ++
+
+        // console.log(playerColourOptions)    
+        randomPlayerColour = playerColourOptions[Math.floor(Math.random() * playerColourOptions.length)] 
+
+        if (playerBrickClasses.includes(targetClassName)) {
+            reducePlayerColourOptions()
+            updateCurrentPlayerColours(randomPlayerColour, targetClassName)
+            event.target.style.backgroundColor = randomPlayerColour
+            checkIfPlayerWon()
+        }
+    }
+}
+
+const bricks = document.querySelectorAll('.brick')
+
+
+for (let i = 0; i < bricks.length; i++) {
+    bricks[i].addEventListener("click", changePlayerBrickColour)
 }
 
 
 middlePanel.addEventListener('click', startGame)
 
-const listOfWrongColours = () => {
-
-    let wrongColours = []
-
-    newPlayerColours.forEach(hexCode => {
-        if (hexCode !== targetColour) {
-            wrongColours.push(hexCode)
-        }
-    })
-
-    return wrongColours
+const updateCurrentPlayerColours = (newColour, targetClassName) => {
+    currentPlayerColours.splice(playerBrickClasses.indexOf(targetClassName), 1, newColour)
 }
- 
-
-const defaultPage = () => {
-
-    shuffle(aiColours)
-    shuffle(playerColours)
-    
-    
-    aiNumberedButtons.forEach((btn, i)=> {
-        document.querySelector(btn).style.backgroundColor = aiColours[i]
-    })
-    
-    playerNumberedButtons.forEach((btn, i)=> {
-        document.querySelector(btn).style.backgroundColor = playerColours[i]
-    })
-}
-
-
-defaultPage()
